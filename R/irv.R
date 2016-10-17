aeb178f3d684d63b8998cb7dca35772d29922a70
Groves <Declan.Groves@allstate.com>
2016-10-17 11:25:48 -0500

    Unimplemeneted irv signature

---
+++ R/irv.R
@@ -0,0 +1,13 @@
+#' Instant runoff voting.
+#'
+#' @param votelist A names list of order-of-preference vote vectors.
+#' @return An IRV object, containing:
+#'
+#'    winner: the winning entry
+#'
+#'    ???: more to come!
+#' @export
+irv <- function(votelist) {
+  invisible()
+}
+
